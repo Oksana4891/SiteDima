da5350b3661369a5ee7efc2ef87c6097f5fed618
Oksana Omelchenko <kuzheloksana@gmail.com>
2021-03-29 12:39:54 +0300

    add animation

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,5 @@
-"use strict";
+'use strict'; // Slick slider
 
-console.log("привет");
 var galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 10,
   slidesPerView: 4,
@@ -17,4 +16,103 @@ var galleryTop = new Swiper('.gallery-top', {
   thumbs: {
     swiper: galleryThumbs
   }
+}); // Open call button
+
+var triggersButton = document.querySelector('.js-call-btn');
+var messengerButtons = document.querySelectorAll('.messenger_bnt');
+
+triggersButton.onclick = function () {
+  messengerButtons.forEach(function (item) {
+    item.classList.toggle('is-open');
+  });
+};
+
+document.body.addEventListener('click', function (e) {
+  if (e.target !== triggersButton && messengerButtons[0].classList.contains('is-open')) {
+    messengerButtons.forEach(function (item) {
+      return item.classList.remove('is-open');
+    });
+  } else {
+    return;
+  }
+}); // fixed menu scrilling
+
+function fixedMenu(menuSelector) {
+  var menu = document.querySelector(menuSelector);
+  window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > menu.getBoundingClientRect().bottom + 30) {
+      menu.classList.add('fixed');
+    } else {
+      menu.classList.remove('fixed');
+    }
+  });
+}
+
+fixedMenu('.js-fixedMenu'); // Scrolling link with requestAnimationFrame
+
+var links = document.querySelectorAll('[href^="#"]'),
+    menuItems = document.querySelectorAll('.js-menuItems'),
+    speed = 0.2;
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    menuItems.forEach(function (item) {
+      return item.classList.remove('is-active');
+    });
+    event.preventDefault();
+    var widthTop = document.documentElement.scrollTop,
+        hash = this.hash,
+        item = link.parentNode,
+        toBlock = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+    item.classList.add('is-active');
+    requestAnimationFrame(step);
+
+    function step(time) {
+      if (start === null) {
+        start = time;
+      }
+
+      var progress = time - start,
+          r = toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock);
+      document.documentElement.scrollTo(0, r);
+
+      if (r != widthTop + toBlock) {
+        requestAnimationFrame(step);
+      } else {
+        location.hash = hash;
+      }
+    }
+  });
 });
+
+function animOnScroll(animSelector, animActiveClass) {
+  console.log('ku-ku');
+  var animItems = document.querySelectorAll(animSelector);
+  console.log(animItems);
+
+  function offsetTop(el) {
+    var rest = el.getBoundingClientRect(),
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+        topHight = rest.top + scrollTop;
+    return topHight;
+  }
+
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', function (e) {
+      animItems.forEach(function (animItem) {
+        var animItemHeight = animItem.offsetHeight;
+        var animItemOffsetTop = offsetTop(animItem);
+        var animStart = 4;
+        var animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (pageYOffset > animItemOffsetTop - animItemPoint && pageYOffset < animItemOffsetTop + animItemHeight) {
+          animItem.classList.add(animActiveClass);
+        } else {
+          animItem.classList.remove(animActiveClass);
+        }
+      });
+    });
+  }
+}
+
+animOnScroll('.js-animate', 'is_active');
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,6 @@
-console.log ("привет");
+'use strict';
+
+// Slick slider
 
 const galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 10,
@@ -15,6 +17,123 @@ const galleryTop = new Swiper('.gallery-top', {
     prevEl: '.swiper-button-prev',
   },
   thumbs: {
-    swiper: galleryThumbs
+    swiper: galleryThumbs,
+  },
+});
+
+// Open call button
+
+const triggersButton = document.querySelector('.js-call-btn');
+const messengerButtons = document.querySelectorAll('.messenger_bnt');
+
+triggersButton.onclick = function () {
+  messengerButtons.forEach(item => {
+    item.classList.toggle('is-open');
+  });
+};
+
+document.body.addEventListener('click', e => {
+  if (
+    e.target !== triggersButton &&
+    messengerButtons[0].classList.contains('is-open')
+  ) {
+    messengerButtons.forEach(item => item.classList.remove('is-open'));
+  } else {
+    return;
   }
 });
+
+// fixed menu scrilling
+
+function fixedMenu(menuSelector) {
+  const menu = document.querySelector(menuSelector);
+  window.addEventListener('scroll', () => {
+    if (
+      document.documentElement.scrollTop >
+      menu.getBoundingClientRect().bottom + 30
+    ) {
+      menu.classList.add('fixed');
+    } else {
+      menu.classList.remove('fixed');
+    }
+  });
+}
+
+fixedMenu('.js-fixedMenu');
+
+// Scrolling link with requestAnimationFrame
+
+const links = document.querySelectorAll('[href^="#"]'),
+  menuItems = document.querySelectorAll('.js-menuItems'),
+  speed = 0.2;
+
+links.forEach(link => {
+  link.addEventListener('click', function (event) {
+    menuItems.forEach(item => item.classList.remove('is-active'));
+    event.preventDefault();
+    let widthTop = document.documentElement.scrollTop,
+      hash = this.hash,
+      item = link.parentNode,
+      toBlock = document.querySelector(hash).getBoundingClientRect().top,
+      start = null;
+
+    item.classList.add('is-active');
+
+    requestAnimationFrame(step);
+
+    function step(time) {
+      if (start === null) {
+        start = time;
+      }
+
+      let progress = time - start,
+        r =
+          toBlock < 0
+            ? Math.max(widthTop - progress / speed, widthTop + toBlock)
+            : Math.min(widthTop + progress / speed, widthTop + toBlock);
+
+      document.documentElement.scrollTo(0, r);
+
+      if (r != widthTop + toBlock) {
+        requestAnimationFrame(step);
+      } else {
+        location.hash = hash;
+      }
+    }
+  });
+});
+
+function animOnScroll(animSelector, animActiveClass) {
+  console.log('ku-ku');
+
+  const animItems = document.querySelectorAll(animSelector);
+  console.log(animItems);
+
+  function offsetTop(el) {
+    const rest = el.getBoundingClientRect(),
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+          topHight = rest.top + scrollTop;
+    return topHight;
+  }
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', e => {
+      animItems.forEach(animItem => {
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffsetTop = offsetTop(animItem);
+        const animStart = 4;
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+        if (
+          pageYOffset > animItemOffsetTop - animItemPoint &&
+          pageYOffset < animItemOffsetTop + animItemHeight
+        ) {
+          animItem.classList.add(animActiveClass);
+        } else {
+          animItem.classList.remove(animActiveClass);
+        }
+      });
+    });
+  }
+}
+
+animOnScroll('.js-animate', 'is_active');
